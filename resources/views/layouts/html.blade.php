9a41a6728b32ae223a36eab89f4c871fe38b6299
Lucas Yang <yangchenshin77@gmail.com>
2023-01-14 23:01:06 +0800

    Change to Bunny Fonts & bump Laravel Mix

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -11,8 +11,8 @@
     <title>@yield('title') - {{ config('app.name') }}</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@300;400;500;700&family=Noto+Sans+TC:wght@300;400;500;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
